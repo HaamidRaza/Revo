05e40a0decbeb5f774980b7496d5da925cb79e2b
HaamidRaza <achatheekhai9@gmail.com>
2025-08-18 18:10:32 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { lazy, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
